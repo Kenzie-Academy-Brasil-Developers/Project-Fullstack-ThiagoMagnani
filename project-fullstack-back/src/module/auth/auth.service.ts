14985ee08e13878d711311aefd4308fa77c4a6d2
Thiago <thiagoarques2@gmail.com>
2024-02-09 03:39:53 -0300

    authentication finished

---
+++ project-fullstack-back/src/module/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dtos/login.dot';
+import { compare } from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login({ email, password }: LoginDto) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    return {
+      token: this.jwtService.sign({ email }, { subject: user.id }),
+      userId: user.id,
+    };
+  }
+}
