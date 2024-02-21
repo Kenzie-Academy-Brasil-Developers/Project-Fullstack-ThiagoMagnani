30142f1cde6ee17afbef7e4c1fc9c959fc86a3b5
Thiago <thiagoarques2@gmail.com>
2024-02-21 20:11:59 -0300

    adjusted

--- project-fullstack-back/src/main.ts
+++ project-fullstack-back/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       transformOptions: { groups: ['transform'] },
     }),
   );
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({ origin: 'https://project-fullstack-thiagomagnani.vercel.app/' });
   await app.listen(3000);
 }
 bootstrap();
