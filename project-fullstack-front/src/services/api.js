4fd4c69bb5f7a5ff92de2453471933f49765b512
Thiago <thiagoarques2@gmail.com>
2024-02-22 16:27:30 -0300

    adjusted

--- project-fullstack-front/src/services/api.js
+++ project-fullstack-front/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://project-fullstack-thiagomagnani-api.onrender.com'
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:3000"
+      : "https://project-fullstack-thiagomagnani-api.onrender.com",
 });
